c12036d23e49a205387db561dd4f0b77ae7a02f8
omiddd <omiddd@NOR>
2020-12-23 09:09:42 +0330

    Exit button callback

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -2,23 +2,54 @@
 #include <stdio.h>
 #include "board.h"
 #include "traverse.h"
+#include <stdlib.h>
 
 #include <FL/Fl.H>
 #include <FL/Fl_Window.H>
 #include <FL/Fl_Box.H>
+#include <FL/Fl_Button.H>
 
 
+void RandomButtonCallback(Fl_Widget* widget, void*){
+    
+    std::cout << "Random\n";
+}
+void OwnButtonCallback(Fl_Widget* , void*){
+    std::cout << "Own\n";
+}
+void ExitButtonCallback(Fl_Widget* , void*){
+    std::cout << "Exit\n";
+    exit(1);
+}
+Fl_Window* mainMenu(){
+  Fl_Window *window = new Fl_Window(250,300,"8Puzzle");
+    //RandomButton
+    Fl_Button *boxRandom = new Fl_Button(75,70,100,30,"Random");
+        
+        boxRandom->box(_FL_ROUND_UP_BOX);
+        boxRandom->labelsize(14);
+
+        //callback
+        boxRandom->callback(RandomButtonCallback);
+        
+    
+    
+    Fl_Button *boxInsert = new Fl_Button(75,110,100,30,"Own");
+    boxInsert->box(_FL_ROUND_UP_BOX);
+    boxInsert->labelsize(14);
+
+
+    Fl_Button *boxExit = new Fl_Button(75,150,100,30,"Exit");
+    boxExit->box(_FL_ROUND_UP_BOX);
+    boxExit->labelsize(14);
+    boxExit->callback(ExitButtonCallback);
+
+  window->end();
+  return window;
+}
 
 int main(int argc , char **argv){
-  Fl_Window *window = new Fl_Window(500,300,"8Puzzle");
-  
-  Fl_Box *boxRandom = new Fl_Box(200,40,100,30,"Random");
-  boxRandom->box(_FL_ROUND_UP_BOX);
-  boxRandom->labelsize(14);
-  Fl_Box *boxInsert = new Fl_Box(200,80,100,30,"Insert");
-  boxInsert->box(_FL_ROUND_UP_BOX);
-  boxInsert->labelsize(14);
-  window->end();
-  window->show(argc, argv);
-  return Fl::run();
+    
+    mainMenu()->show(argc, argv);
+    return Fl::run();
 }
